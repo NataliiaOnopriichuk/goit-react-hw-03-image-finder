4dc0185387df1453f0d4f98d274248f01fafeab0
DemoNataliia <nataliam110388@gmail.com>
2023-01-25 22:41:57 +0200

    init8

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,19 +5,19 @@ import axios from "axios";
 const API_KEY = "32288406-b66d59f2ed44691f7d029918e";
 axios.defaults.baseURL = "https://pixabay.com";
 
-export const getSearchedNewsApi = (query, page) => {
-  return axios
-    .get("/api", {
+
+export const getSearchedNewsApi = async (query, page) => {
+  return await axios
+    .get('/api', {
       params: {
-        key: API_KEY,
         q: query,
         page,
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
         per_page: 12,
-        image_type: "photo",
-        orientation: "horizontal",
       },
     })
     .then((response) => response.data);
 };
 
-// /?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
